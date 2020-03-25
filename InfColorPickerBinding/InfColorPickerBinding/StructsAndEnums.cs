755d07c4cd1c5ea2d9075edccbcd3f44ff727888
jair.pinedo <jair.pinedo@unosquare.com>
2020-03-25 09:53:29 -0600

    Init project

---
+++ InfColorPickerBinding/InfColorPickerBinding/StructsAndEnums.cs
@@ -0,0 +1,12 @@
+using System.Runtime.InteropServices;
+using CoreGraphics;
+
+namespace InfColorPicker
+{
+	public enum InfComponentIndex : uint
+	{
+		Hue = 0,
+		Saturation = 1,
+		Brightness = 2
+	}
+}
---
+++ InfColorPickerSample/InfColorPickerSample/ViewController.cs
@@ -0,0 +1,36 @@
+﻿using Foundation;
+using System;
+using UIKit;
+using InfColorPicker;
+
+namespace InfColorPickerSample
+{
+    public partial class ViewController : UIViewController
+    {
+        ColorSelectedDelegate selector;
+
+        public ViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            ChangeColorButton.TouchUpInside += HandleTouchUpInsideWithStrongDelegate;
+            selector = new ColorSelectedDelegate(this);
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+
+        private void HandleTouchUpInsideWithStrongDelegate(object sender, EventArgs e)
+        {
+            InfColorPickerController picker = InfColorPickerController.ColorPickerViewController;
+            picker.Delegate = selector;
+            picker.PresentModallyOverViewController(this);
+        }
+    }
+}
